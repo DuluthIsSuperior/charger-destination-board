c36a502ee3b34207807f4bfeebc053b9801778b1
Kyle <kyle.i.dick@outlook.com>
2021-01-27 15:59:46 -0500

    Transitioning to storing graphics on flash to resolve memory issues

--- character.h
+++ character.h
@@ -5,6 +5,7 @@
 class Character {
   public:
     char** lines; // using char for 8 bit integer
+    int* linesPROGMEM = NULL; // use to store the beginning pointer to the first point
     int sizeOfLines;
     char symbol;
     Adafruit_SSD1331* display;
@@ -27,9 +28,17 @@ Character::draw(int x, int y, int color) {
 //    Serial.println(sizeOfLines);
 //    display->print(buf);
 //  }
-  for (int i = 0; i < sizeOfLines; i++) {
-    char* line = lines[i];
-    display->drawLine(line[0] + x, line[1] + y, line[2] + x, line[3] + y, color);
+  if (linesPROGMEM == NULL) {
+    for (int i = 0; i < sizeOfLines; i++) {
+      char* line = lines[i];
+      display->drawLine(line[0] + x, line[1] + y, line[2] + x, line[3] + y, color);
+    }
+  } else {
+    int dots = sizeOfLines * 4;
+    for (int i = 0; i < dots; i += 4) {
+      int* p = linesPROGMEM + i;
+      display->drawLine(pgm_read_word(&p[0]) + x, pgm_read_word(&p[1]) + y, pgm_read_word(&p[2]) + x, pgm_read_word(&p[3]) + y, color);
+    }
   }
 };
 
