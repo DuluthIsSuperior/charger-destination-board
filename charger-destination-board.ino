96b70a0f828c95ffba60e33d451c0134afda20d0
Kyle <kyle.i.dick@outlook.com>
2021-01-27 22:06:37 -0500

    Inverted board to prototype; Implemented scrolling

--- charger-destination-board.ino
+++ charger-destination-board.ino
@@ -2,30 +2,60 @@
 
 const int TOP_LEFT_CORNER[] = {0, 0};
 const int BOTTOM_RIGHT_CORNER[] = {49, 14};
+  
+const int AMBER = 0xFFB400;
+const int BLACK = 0x000000;
 
+void clearBoard() {
+  display.fillRect(TOP_LEFT_CORNER[0], TOP_LEFT_CORNER[1], BOTTOM_RIGHT_CORNER[0], BOTTOM_RIGHT_CORNER[1], BLACK); // start x, start y, end x, end y  
+}
+
+int x = 1;
 void setup() {
   Serial.begin(9600);
   
-  const int AMBER = 0xFFB400;
-  const int BLACK = 0x000000;
-  
   display.begin();
   display.fillScreen(0x0);
-  display.fillRect(TOP_LEFT_CORNER[0], TOP_LEFT_CORNER[1], BOTTOM_RIGHT_CORNER[0], BOTTOM_RIGHT_CORNER[1], AMBER); // start x, start y, end x, end y
-
-  struct Map char_map;
-  char str[] = "KYLE DICK";
-  int x = 1;
-  int y = 3;
-  for (int i = 0; i < sizeof(str) / sizeof(char) - 1; i++) {
-    struct Character c = char_map.getCharacter(str[i]);
-    c.draw(x, y, BLACK);
-    x += c.width + 1;
-  }
+  display.drawRect(TOP_LEFT_CORNER[0], TOP_LEFT_CORNER[1], BOTTOM_RIGHT_CORNER[0] + 1, BOTTOM_RIGHT_CORNER[1] + 1, AMBER);
+  clearBoard();
 }
 
-bool scrolling = false;
+bool scrolling = true;
+bool messageChanged = true; // initalized to true to let the message initially show up
+char str[50] = "PERE MARQUETTE";
+int y = 3;
+struct Map char_map;
+long lastMoved = millis();
 
 void loop() {
-  
+  if (messageChanged) {
+    clearBoard();
+    x = scrolling ? BOTTOM_RIGHT_CORNER[0] + 3 : 1;
+    int tempX = x;
+    for (int i = 0; i < sizeof(str) / sizeof(char) - 1; i++) {
+      if (str[i] != 0) {
+        struct Character c = char_map.getCharacter(str[i]);
+        c.draw(tempX, y, AMBER);
+        tempX += c.width + 1;
+      } else {
+        break;
+      }
+    }
+    messageChanged = false;
+  }
+  if (scrolling && millis() - lastMoved >= 50) {
+    clearBoard();
+    x--;
+    int tempX = x;
+    for (int i = 0; i < sizeof(str) / sizeof(char) - 1; i++) {
+      if (str[i] != 0) {
+        struct Character c = char_map.getCharacter(str[i]);
+        c.draw(tempX, y, AMBER);
+        tempX += c.width + 1;
+      } else {
+        break;
+      }
+    }
+    lastMoved = millis();
+  }
 }
